b115b8f0ca394a333f17e567c972daeac457a585
XiamiYoung <young.xue@sap.com>
2021-09-04 15:15:34 +0800

    local font

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'babel-polyfill'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 import 'vuetify/dist/vuetify.min.css';
 import Vuetify from 'vuetify';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import './styles/global.css';
 import VuetifyToast from 'vuetify-toast-snackbar'
 import axios from 'axios'
@@ -19,7 +19,9 @@ import 'vue-loading-overlay/dist/vue-loading.css';
 import store from '@/store/store';
 
 setupComponents(Vue);
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  iconfont: 'md'
+})
 Vue.use(VueAxios, axios);
 Vue.use(Loading);
 Vue.use(VuetifyToast, {
